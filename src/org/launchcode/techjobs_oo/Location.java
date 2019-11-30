a1207845ab03d86a9543cc0a0e6ccb3b6c49a3d2
Krystal Farwig <youngshaowei@gmail.com>
2019-11-29 19:26:51 -0600

    Completed first two tasks. Location, core competency, and position type have needed constructors and methods. Getters and setters are in place.

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -12,6 +12,10 @@ public class Location {
         nextId++;
     }
 
+    public Location(String value) {
+        this();
+        this.value = value;
+    }
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
